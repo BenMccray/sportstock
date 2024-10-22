29db04de11223733a7669393127a6494cfeabc42
BenMccray <113936697+BenMccray@users.noreply.github.com>
2024-10-22 16:33:32 -0400

    move to desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{js,jsx,ts,tsx,css,scss,sass}"],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+  darkMode: "selector",
+  // ...
 };
